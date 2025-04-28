3014b896a9842554e13092af4a6e1bf80cf8cbdb
Pedro Candido Salvio <pedrocsalvio@gmail.com>
2025-04-28 20:43:16 -0300

    refactor: improving creating valid character test, using weapon as object

--- src/test/java/br/ifsp/rpg/RpgTest.java
+++ src/test/java/br/ifsp/rpg/RpgTest.java
@@ -7,12 +7,13 @@ import org.junit.jupiter.api.Test;
 public class RpgTest {
 
     @Test
-    public void creatingCharcaterTest() {
-        RpgCharacter character = new RpgCharacter("Character", "Warrior", "Orc", "Axe");
+    public void creatingValidCharcaterTest() {
+        Weapon axe = new Weapon("Axe", 2, 6);
+        RpgCharacter character = new RpgCharacter("Character", "Warrior", "Orc", axe);
 
         assertThat(character.getId()).isNotNull();
         assertThat(character.getName()).isEqualTo("Character");
         assertThat(character.getRace()).isEqualTo("Orc");
-        assertThat(character.getWeapon()).isEqualTo("Axe");
+        assertThat(character.getWeapon().getName()).isEqualTo(axe.getName());
     }
 }
