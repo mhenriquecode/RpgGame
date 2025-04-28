fd88792f16c951abb8acf063c31ff8de8b891aca
Pedro Candido Salvio <pedrocsalvio@gmail.com>
2025-04-28 19:13:26 -0300

    test: (red) create character test

--- src/test/java/br/ifsp/rpg/RpgTest.java
+++ src/test/java/br/ifsp/rpg/RpgTest.java
@@ -4,4 +4,15 @@ import org.junit.jupiter.api.Test;
 
 public class RpgTest {
 
+    @Test
+    public void creatingCharcaterTest() {
+        Race orc = new Race("Orc", 0, 5, 0, 0);
+        Weapon axe = new Weapon("Axe", 2, 6);
+        Character character = new Character("Character", ClassType.WARRIOR, orc, axe);
+
+        assertThat(character.getId()).isNotNull;
+        assertThat(character.getName()).isEqualTo("Character");
+        assertThat(character.getRace()).isEqualTo(orc);
+        assertThat(character.getWeapon()).isEqualTo(axe);
+    }
 }
