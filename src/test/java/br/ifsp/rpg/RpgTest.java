0e4927741f01f15391aafdf867e7bf7cf218b9d9
Pedro Candido Salvio <pedrocsalvio@gmail.com>
2025-04-28 20:00:25 -0300

    feat: (green) implements rpgcharacter in RpgCharacter class

--- src/test/java/br/ifsp/rpg/RpgTest.java
+++ src/test/java/br/ifsp/rpg/RpgTest.java
@@ -1,18 +1,18 @@
 package br.ifsp.rpg;
 import static org.assertj.core.api.Assertions.*;
+
+import br.ifsp.rpg.model.RpgCharacter;
 import org.junit.jupiter.api.Test;
 
 public class RpgTest {
 
     @Test
     public void creatingCharcaterTest() {
-        Race orc = new Race("Orc", 0, 5, 0, 0);
-        Weapon axe = new Weapon("Axe", 2, 6);
-        Character character = new Character("Character", ClassType.WARRIOR, orc, axe);
+        RpgCharacter character = new RpgCharacter("Character", "Warrior", "Orc", "Axe");
 
-        assertThat(character.getId()).isNotNull;
+        assertThat(character.getId()).isNotNull();
         assertThat(character.getName()).isEqualTo("Character");
-        assertThat(character.getRace()).isEqualTo(orc);
-        assertThat(character.getWeapon()).isEqualTo(axe);
+        assertThat(character.getRace()).isEqualTo("Orc");
+        assertThat(character.getWeapon()).isEqualTo("Axe");
     }
 }
