4e3a00e75d8d278272306c1e278f7de1f5808e15
Matheus Carvalho <matheucar186@gmail.com>
2025-04-29 13:28:21 -0300

    refector (combat): adding tiebreaker logic in the getFistToPlay method

--- src/main/java/br/ifsp/rpg/model/Combat.java
+++ src/main/java/br/ifsp/rpg/model/Combat.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Combat {
     private final RpgCharacter player1;
     private final RpgCharacter player2;
+    private final Random random = new Random();
 
     public Combat(RpgCharacter player1, RpgCharacter player2) {
         this.player1 = player1;
@@ -14,8 +15,12 @@ public class Combat {
     public RpgCharacter getFirstToPlay() {
         if (player1.getSpeed() > player2.getSpeed()) {
             return player1;
+        } else if (player2.getSpeed() > player1.getSpeed()) {
+            return player2;
+        } else {
+            int d2 = random.nextInt(2) + 1;
+            return d2 == 1 ? player1 : player2;
         }
-        return player2;
     }
 
 }
