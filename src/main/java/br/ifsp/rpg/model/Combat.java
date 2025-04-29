14675bc48f4fcbf1ab5ca157a66bcb1a866eff6f
Matheus Carvalho <matheucar186@gmail.com>
2025-04-29 12:53:43 -0300

    feat (combat): adding the getFirstToPlay method

--- src/main/java/br/ifsp/rpg/model/Combat.java
+++ src/main/java/br/ifsp/rpg/model/Combat.java
@@ -1,5 +1,7 @@
 package br.ifsp.rpg.model;
 
+import java.util.Random;
+
 public class Combat {
     private final RpgCharacter player1;
     private final RpgCharacter player2;
@@ -8,4 +10,12 @@ public class Combat {
         this.player1 = player1;
         this.player2 = player2;
     }
+
+    public RpgCharacter getFirstToPlay() {
+        if (player1.getSpeed() > player2.getSpeed()) {
+            return player1;
+        }
+        return player2;
+    }
+
 }
