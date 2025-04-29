4a3e380fef200f289f10b40a37e37546d85c1fa3
Matheus Carvalho <matheucar186@gmail.com>
2025-04-29 12:02:10 -0300

    feat (combat): creating the class combat

---
+++ src/main/java/br/ifsp/rpg/model/Combat.java
@@ -0,0 +1,11 @@
+package br.ifsp.rpg.model;
+
+public class Combat {
+    private final RpgCharacter player1;
+    private final RpgCharacter player2;
+
+    public Combat(RpgCharacter player1, RpgCharacter player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+}
