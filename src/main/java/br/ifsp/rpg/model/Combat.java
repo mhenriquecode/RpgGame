949713198a008523c5bfded77d5fe97d0debe734
Matheus Carvalho <matheucar186@gmail.com>
2025-04-30 17:09:03 -0300

    refactor: adding method overloading in Combat class for testability

--- src/main/java/br/ifsp/rpg/model/Combat.java
+++ src/main/java/br/ifsp/rpg/model/Combat.java
@@ -7,11 +7,18 @@ import java.util.Random;
 public class Combat {
     private final RpgCharacter player1;
     private final RpgCharacter player2;
-    private final Random random = new Random();
+    private final Random random;
 
     public Combat(RpgCharacter player1, RpgCharacter player2) {
         this.player1 = player1;
         this.player2 = player2;
+        this.random = new Random();
+    }
+
+    public Combat(RpgCharacter player1, RpgCharacter player2, Random random) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.random = random;
     }
 
     public void start(){
@@ -32,20 +39,14 @@ public class Combat {
         }
     }
 
-
     public RpgCharacter getFirstToPlay() {
         if (player1.getSpeed() > player2.getSpeed()) {
             return player1;
         } else if (player2.getSpeed() > player1.getSpeed()) {
             return player2;
         } else {
-            int d2 = rollD2();
-            return d2 == 1 ? player1 : player2;
+            return random.nextInt(2) == 0 ? player1 : player2;
         }
-    }
-
-    public int rollD2() {
-        return random.nextInt(2) + 1;
     }
 
 }
