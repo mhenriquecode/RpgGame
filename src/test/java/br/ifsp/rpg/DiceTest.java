9c48a3f2bdb4117a9c40e0640d342c1a5085a921
Matheus Carvalho <matheucar186@gmail.com>
2025-04-30 16:10:19 -0300

    test (red): should return sum of simulated values for weapon

--- src/test/java/br/ifsp/rpg/DiceTest.java
+++ src/test/java/br/ifsp/rpg/DiceTest.java
@@ -2,7 +2,9 @@ package br.ifsp.rpg;
 
 import br.ifsp.rpg.interfaces.DiceRoll;
 import br.ifsp.rpg.model.RollHitDice;
+import br.ifsp.rpg.model.Weapon;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.Random;
@@ -13,6 +15,8 @@ import static org.mockito.Mockito.*;
 public class DiceTest {
 
     @Test
+    @Tag("Unit Test")
+    @Tag("TDD")
     @DisplayName("hit dice should return minimum value")
     void hitDiceShouldReturnMinimumValue(){
         Random mockRandom = mock(Random.class);
@@ -23,5 +27,23 @@ public class DiceTest {
 
         assertEquals(1, result);
         verify(mockRandom).nextInt(20);
+    }
+
+    @Test
+    @Tag("Unit Test")
+    @Tag("TDD")
+    @DisplayName("should return sum of simulated values for weapon")
+    void shouldReturnSumofSimulatedValuesForWeapon(){
+        Weapon weapon = new Weapon("testeWeapn", 2, 6);
+
+        Random mockRandom = mock(Random.class);
+        when(mockRandom.nextInt(6)).thenReturn(0, 5);
+
+        DiceRoll attackDice = new AttackDice(weapon, mockRandom);
+        int result = attackDice.roll();
+
+        assertEquals(6, result);
+        verify(mockRandom, timeout(2)).nextInt(6);
+
     }
 }
