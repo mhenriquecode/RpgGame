d21ba869dc65190260fc2a1b2886358f17be87d4
Matheus Carvalho <matheucar186@gmail.com>
2025-05-01 02:31:32 -0300

    feat (green): implementing execute method and attack method

--- src/main/java/br/ifsp/rpg/model/Turn.java
+++ src/main/java/br/ifsp/rpg/model/Turn.java
@@ -3,10 +3,20 @@ package br.ifsp.rpg.model;
 public class Turn {
     private final RpgCharacter current;
     private final RpgCharacter opponent;
+    private RollHitDice hitDice;
 
     public Turn(RpgCharacter current, RpgCharacter opponent) {
         this.current = current;
         this.opponent = opponent;
+    }
+
+    public void execute(int actionChoice) {
+        if (actionChoice == 1) {
+            attack();
+        }
+    }
+
+    public void attack() {
     }
 
 }
