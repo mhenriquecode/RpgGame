5e9b2acc3ff0a791a4698cd6ea7e8111b41c91e9
Matheus Carvalho <matheucar186@gmail.com>
2025-05-04 18:30:58 -0300

    refactor: moving Special Effects Tests for RpgCharacterTest class

--- src/test/java/br/ifsp/rpg/RpgCharacterTest.java
+++ src/test/java/br/ifsp/rpg/RpgCharacterTest.java
@@ -8,12 +8,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class RpgCharacterTest {
     private RpgCharacter player1;
+    @Mock private Random mockRandom = mock(Random.class);
 
     @BeforeEach
     void setUp() {
@@ -188,5 +193,60 @@ public class RpgCharacterTest {
 
         assertEquals(expectedMaxHealth, character.getMaxHealth());
         assertEquals(expectedDefense, character.getDefense());
+    }
+
+    @Test
+    @Tag("Unit-test")
+    @Tag("TDD")
+    @DisplayName("Applying berserk special attack effect test")
+    void applyingBerserkSpecialAttackEffectTest(){
+        when(mockRandom.nextInt(100)).thenReturn(5);
+
+        RpgCharacter player = new RpgCharacter("Character", ClassType.BERSERK, Race.ORC, Weapon.AXE, mockRandom);
+
+        assertThat(player.getClassType()).isEqualTo(ClassType.BERSERK);
+        assertThat(player.attack()).isGreaterThanOrEqualTo(player.getStrength() + 2 * 2);
+    }
+
+    @Test
+    @Tag("Unit-test")
+    @Tag("TDD")
+    @DisplayName("Applying warrior special attack effect test")
+    void applyingWarriorSpecialAttackEffectTest(){
+        when(mockRandom.nextInt(100)).thenReturn(5);
+
+        RpgCharacter player = new RpgCharacter("Character", ClassType.WARRIOR, Race.HUMAN, Weapon.SWORD, mockRandom);
+        int oldDefense = player.getDefense();
+        int damage = player.attack();
+
+        assertThat(player.getClassType()).isEqualTo(ClassType.WARRIOR);
+        assertThat(player.getDefense()).isEqualTo(oldDefense + damage);
+    }
+
+    @Test
+    @Tag("Unit-test")
+    @Tag("TDD")
+    @DisplayName("Applying paladin special attack effect test")
+    void applyingPaladinSpecialAttackEffectTest() {
+        when(mockRandom.nextInt(100)).thenReturn(5);
+
+        RpgCharacter player = new RpgCharacter("Character", ClassType.PALADIN, Race.HUMAN, Weapon.SWORD, mockRandom);
+        player.attack();
+
+        assertThat(player.getClassType()).isEqualTo(ClassType.PALADIN);
+        assertThat(player.getHealth()).isGreaterThanOrEqualTo(118);
+    }
+
+    @Test
+    @Tag("Unit-test")
+    @Tag("TDD")
+    @DisplayName("Applying duelist special attack effect test")
+    void applyingDuelistSpecialAttackEffectTest() {
+        when(mockRandom.nextInt(100)).thenReturn(5);
+
+        RpgCharacter player = new RpgCharacter("Character", ClassType.DUELIST, Race.HUMAN, Weapon.SWORD, mockRandom);
+
+        assertThat(player.getClassType()).isEqualTo(ClassType.DUELIST);
+        assertThat(player.attack()).isStrictlyBetween(23, 41);
     }
 }
