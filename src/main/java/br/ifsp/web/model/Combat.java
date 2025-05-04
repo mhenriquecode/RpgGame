32ae485bf7e94dc83538caabfa3a3048b2e4b293
Matheus Carvalho <matheucar186@gmail.com>
2025-05-03 23:20:04 -0300

    feat (green): implementing looping and winner logic in the Combat class

--- src/main/java/br/ifsp/web/model/Combat.java
+++ src/main/java/br/ifsp/web/model/Combat.java
@@ -1,11 +1,17 @@
 package br.ifsp.web.model;
 
+import br.ifsp.web.interfaces.ChooseAction;
+import br.ifsp.web.model.actions.ChooseUserAction;
+import lombok.Getter;
+
 import java.util.Random;
 
+@Getter
 public class Combat {
     private final RpgCharacter player1;
     private final RpgCharacter player2;
     private final Random random;
+    private RpgCharacter winner;
 
     public Combat(RpgCharacter player1, RpgCharacter player2) {
         this.player1 = player1;
@@ -20,12 +26,23 @@ public class Combat {
     }
 
     public void start(){
-        RpgCharacter faster = getFirstToPlay();
-        RpgCharacter slower = faster == player1 ? player2 : player1;
+        RpgCharacter current = getFirstToPlay();
+        RpgCharacter opponent = current == player1 ? player2 : player1;
 
+        while (player1.getHealth() > 0 && player2.getHealth() > 0) {
+            ChooseAction currentStrategy = new ChooseUserAction(1);
 
+            Turn turn = new Turn(current, opponent, currentStrategy);
+            turn.execute();
 
-
+            if (opponent.getHealth() <= 0) {
+                winner = current;
+                break;
+            }
+            RpgCharacter temp = current;
+            current = opponent;
+            opponent = temp;
+        }
     }
 
     public RpgCharacter getFirstToPlay() {
