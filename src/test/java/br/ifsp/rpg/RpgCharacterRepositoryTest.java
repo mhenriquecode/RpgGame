97b293790760deca25048e7da4181a41c1cc459b
Matheus Carvalho <matheucar186@gmail.com>
2025-05-05 00:15:00 -0300

    style: created nested for TDD tests and Unit-tests for RpgCharacterRepositoryTest class

--- src/test/java/br/ifsp/rpg/RpgCharacterRepositoryTest.java
+++ src/test/java/br/ifsp/rpg/RpgCharacterRepositoryTest.java
@@ -8,10 +8,7 @@ import br.ifsp.web.model.enums.Race;
 import br.ifsp.web.model.RpgCharacter;
 import br.ifsp.web.model.enums.Weapon;
 import br.ifsp.web.service.CharacterService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -38,146 +35,147 @@ public class RpgCharacterRepositoryTest {
         character = service.create(new CharacterDTO("Character", ClassType.DUELIST, Race.HUMAN, Weapon.DAGGER));
     }
 
-    @Test
-    @Tag("Unit-test")
-    @Tag("TDD")
-    @DisplayName("Find character by id test")
-    void findCharacterByIdTest(){
-        when(repository.findById(character.getId())).thenReturn(Optional.of(character));
-        assertThat(service.getCharacter(character.getId())).isEqualTo(Optional.of(character));
+    @Nested
+    @DisplayName("TDD Characters repository test")
+    class TddCharactersRepositoryTest {
+        @Test
+        @Tag("Unit-test")
+        @Tag("TDD")
+        @DisplayName("Find character by id test")
+        void findCharacterByIdTest(){
+            when(repository.findById(character.getId())).thenReturn(Optional.of(character));
+            assertThat(service.getCharacter(character.getId())).isEqualTo(Optional.of(character));
+        }
+        @Test
+        @Tag("Unit-test")
+        @Tag("TDD")
+        @DisplayName("Get all characters test")
+        void getAllCharactersTest(){
+            RpgCharacter character2 = new RpgCharacter("Character2", ClassType.WARRIOR, Race.DWARF, Weapon.HAMMER);
+            List<RpgCharacter> mockList = List.of(character, character2);
+
+            when(repository.findAll()).thenReturn(mockList);
+
+            List<RpgCharacter> result = service.getAllCharacters();
+            assertThat(result).hasSize(2).containsExactlyInAnyOrder(character, character2);
+        }
+        @Test
+        @Tag("Unit-test")
+        @Tag("TDD")
+        @DisplayName("Save character test")
+        void saveCharacterTest(){
+            UUID fakeId = UUID.randomUUID();
+
+            when(repository.save(any(RpgCharacter.class))).thenAnswer(invocation -> {
+                RpgCharacter saved = invocation.getArgument(0);
+                saved.setId(fakeId);
+                return saved;
+            });
+
+            when(repository.findById(fakeId)).thenReturn(Optional.of(character));
+
+            RpgCharacter savedCharacter = service.create(new CharacterDTO(
+                    "Character", ClassType.WARRIOR, Race.HUMAN, Weapon.HAMMER));
+
+            savedCharacter.setId(fakeId);
+
+            assertThat(savedCharacter.getId()).isNotNull();
+            assertThat(service.getCharacter(savedCharacter.getId())).isEqualTo(Optional.of(character));
+        }
+        @Test
+        @Tag("Unit-test")
+        @Tag("TDD")
+        @DisplayName("Update character test")
+        void updateCharacterTest(){
+            when(repository.findById(character.getId())).thenReturn(Optional.of(character));
+
+            service.update(character.getId(), new CharacterDTO("New Name", ClassType.PALADIN, Race.ORC, Weapon.SWORD));
+
+            assertThat(character.getName()).isEqualTo("New Name");
+            assertThat(character.getClassType()).isEqualTo(ClassType.PALADIN);
+            assertThat(character.getRace()).isEqualTo(Race.ORC);
+            assertThat(character.getWeapon()).isEqualTo(Weapon.SWORD);
+
+            verify(repository).save(character);
+        }
+        @Test
+        @Tag("Unit-test")
+        @Tag("TDD")
+        @DisplayName("Delete existing character test")
+        void deleteExistingCharacterTest(){
+            when(repository.findById(Mockito.any())).thenReturn(Optional.of(character));
+            service.delete(character.getId());
+            verify(repository).delete(character);
+        }
+
     }
 
-    @Test
-    @Tag("Unit-test")
-    @DisplayName("Trying to find character that does not exists test")
-    void findCharacterThatDoesNotExistTest(){
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> service.getCharacter(UUID.randomUUID()));
-    }
+    @Nested
+    @DisplayName("Unit Characters repository test")
+    class UnitCharactersRepositoryTest {
+        @Test
+        @Tag("Unit-test")
+        @DisplayName("Trying to find character that does not exists test")
+        void findCharacterThatDoesNotExistTest(){
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> service.getCharacter(UUID.randomUUID()));
+        }
+        @Test
+        @Tag("Unit-test")
+        @DisplayName("Return empty list when repository is empty")
+        void getAllCharactersEmptyTest(){
+            assertThat(service.getAllCharacters()).isEmpty();
+        }
+        @Test
+        @Tag("Unit-test")
+        @DisplayName("Trying to save invalid weapon character test")
+        void tryingToSaveInvalidWeaponCharacterTest(){
+            assertThatExceptionOfType(NullPointerException.class)
+                    .isThrownBy(() -> service.save(new RpgCharacter(
+                            "New Character", ClassType.BERSERK, Race.HUMAN,null)));
+        }
 
-    @Test
-    @Tag("Unit-test")
-    @Tag("TDD")
-    @DisplayName("Get all characters test")
-    void getAllCharactersTest(){
-        RpgCharacter character2 = new RpgCharacter("Character2", ClassType.WARRIOR, Race.DWARF, Weapon.HAMMER);
-        List<RpgCharacter> mockList = List.of(character, character2);
+        @Test
+        @Tag("Unit-test")
+        @DisplayName("Trying to save invalid race character test")
+        void tryingToSaveInvalidRaceCharacterTest(){
+            assertThatExceptionOfType(NullPointerException.class)
+                    .isThrownBy(() -> service.save(new RpgCharacter(
+                            "New Character", ClassType.BERSERK, null,Weapon.DAGGER)));
+        }
 
-        when(repository.findAll()).thenReturn(mockList);
+        @Test
+        @Tag("Unit-test")
+        @DisplayName("Trying to save invalid class type character test")
+        void tryingToSaveInvalidClassTypeCharacterTest(){
+            assertThatExceptionOfType(NullPointerException.class)
+                    .isThrownBy(() -> service.save(new RpgCharacter(
+                            "New Character", null, Race.HUMAN,Weapon.HAMMER)));
+        }
 
-        List<RpgCharacter> result = service.getAllCharacters();
-        assertThat(result).hasSize(2).containsExactlyInAnyOrder(character, character2);
-    }
-
-    @Test
-    @Tag("Unit-test")
-    @DisplayName("Return empty list when repository is empty")
-    void getAllCharactersEmptyTest(){
-        assertThat(service.getAllCharacters()).isEmpty();
-    }
-
-    @Test
-    @Tag("Unit-test")
-    @Tag("TDD")
-    @DisplayName("Save character test")
-    void saveCharacterTest(){
-        UUID fakeId = UUID.randomUUID();
-
-        when(repository.save(any(RpgCharacter.class))).thenAnswer(invocation -> {
-            RpgCharacter saved = invocation.getArgument(0);
-            saved.setId(fakeId);
-            return saved;
-        });
-
-        when(repository.findById(fakeId)).thenReturn(Optional.of(character));
-
-        RpgCharacter savedCharacter = service.create(new CharacterDTO(
-                "Character", ClassType.WARRIOR, Race.HUMAN, Weapon.HAMMER));
-
-        savedCharacter.setId(fakeId);
-
-        assertThat(savedCharacter.getId()).isNotNull();
-        assertThat(service.getCharacter(savedCharacter.getId())).isEqualTo(Optional.of(character));
-    }
-
-    @Test
-    @Tag("Unit-test")
-    @DisplayName("Trying to save invalid weapon character test")
-    void tryingToSaveInvalidWeaponCharacterTest(){
-        assertThatExceptionOfType(NullPointerException.class)
-                .isThrownBy(() -> service.save(new RpgCharacter(
-                        "New Character", ClassType.BERSERK, Race.HUMAN,null)));
-    }
-
-    @Test
-    @Tag("Unit-test")
-    @DisplayName("Trying to save invalid race character test")
-    void tryingToSaveInvalidRaceCharacterTest(){
-        assertThatExceptionOfType(NullPointerException.class)
-                .isThrownBy(() -> service.save(new RpgCharacter(
-                        "New Character", ClassType.BERSERK, null,Weapon.DAGGER)));
-    }
-
-    @Test
-    @Tag("Unit-test")
-    @DisplayName("Trying to save invalid class type character test")
-    void tryingToSaveInvalidClassTypeCharacterTest(){
-        assertThatExceptionOfType(NullPointerException.class)
-                .isThrownBy(() -> service.save(new RpgCharacter(
-                        "New Character", null, Race.HUMAN,Weapon.HAMMER)));
-    }
-
-    @Test
-    @Tag("Unit-test")
-    @DisplayName("Trying to save invalid name character test")
-    void tryingToSaveInvalidNameCharacterTest(){
-        assertThatExceptionOfType(NullPointerException.class)
-                .isThrownBy(() -> service.save(new RpgCharacter(
-                        null, ClassType.BERSERK, Race.HUMAN,Weapon.DAGGER)));
-    }
-
-    @Test
-    @Tag("Unit-test")
-    @Tag("TDD")
-    @DisplayName("Update character test")
-    void updateCharacterTest(){
-        when(repository.findById(character.getId())).thenReturn(Optional.of(character));
-
-        service.update(character.getId(), new CharacterDTO("New Name", ClassType.PALADIN, Race.ORC, Weapon.SWORD));
-
-        assertThat(character.getName()).isEqualTo("New Name");
-        assertThat(character.getClassType()).isEqualTo(ClassType.PALADIN);
-        assertThat(character.getRace()).isEqualTo(Race.ORC);
-        assertThat(character.getWeapon()).isEqualTo(Weapon.SWORD);
-
-        verify(repository).save(character);
-    }
-
-    @Test
-    @Tag("Unit-test")
-    @DisplayName("Trying to update non existing character")
-    void tryingToUpdateNonExistingCharacterTest(){
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> service.update(
-                        UUID.randomUUID(),
-                        new CharacterDTO("New Name", ClassType.WARRIOR, Race.ORC, Weapon.HAMMER)));
-    }
-
-    @Test
-    @Tag("Unit-test")
-    @Tag("TDD")
-    @DisplayName("Delete existing character test")
-    void deleteExistingCharacterTest(){
-        when(repository.findById(Mockito.any())).thenReturn(Optional.of(character));
-        service.delete(character.getId());
-        verify(repository).delete(character);
-    }
-
-    @Test
-    @Tag("Unit-test")
-    @DisplayName("Delete non existing character test")
-    void deleteNonExistingCharacterTest(){
-        assertThatExceptionOfType(CharacterNotFoundException.class)
-                .isThrownBy(() -> service.delete(UUID.randomUUID()));
+        @Test
+        @Tag("Unit-test")
+        @DisplayName("Trying to save invalid name character test")
+        void tryingToSaveInvalidNameCharacterTest(){
+            assertThatExceptionOfType(NullPointerException.class)
+                    .isThrownBy(() -> service.save(new RpgCharacter(
+                            null, ClassType.BERSERK, Race.HUMAN,Weapon.DAGGER)));
+        }
+        @Test
+        @Tag("Unit-test")
+        @DisplayName("Trying to update non existing character")
+        void tryingToUpdateNonExistingCharacterTest(){
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> service.update(
+                            UUID.randomUUID(),
+                            new CharacterDTO("New Name", ClassType.WARRIOR, Race.ORC, Weapon.HAMMER)));
+        }
+        @Test
+        @Tag("Unit-test")
+        @DisplayName("Delete non existing character test")
+        void deleteNonExistingCharacterTest(){
+            assertThatExceptionOfType(CharacterNotFoundException.class)
+                    .isThrownBy(() -> service.delete(UUID.randomUUID()));
+        }
     }
 }
