c6359c8c93ecf1291f19619a34ec0bc239f5cac2
Matheus Carvalho <matheucar186@gmail.com>
2025-05-05 00:20:48 -0300

    style: created nested for TDD tests and Unit-tests for DiceTest class

--- src/test/java/br/ifsp/rpg/DiceTest.java
+++ src/test/java/br/ifsp/rpg/DiceTest.java
@@ -4,10 +4,7 @@ import br.ifsp.web.interfaces.DiceRoll;
 import br.ifsp.web.model.dice.RollAttackDice;
 import br.ifsp.web.model.dice.RollHitDice;
 import br.ifsp.web.model.enums.Weapon;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.Random;
 
@@ -22,83 +19,97 @@ public class DiceTest {
         mockRandom = mock(Random.class);
     }
 
-    @Test
-    @Tag("Unit-test")
-    @Tag("TDD")
-    @DisplayName("hit dice should return minimum value")
-    void hitDiceShouldReturnMinimumValue(){
-        when(mockRandom.nextInt(20)).thenReturn(0);
 
-        DiceRoll hitDice = new RollHitDice(mockRandom);
-        int result = hitDice.roll();
+    @Nested
+    @DisplayName("TDD dice test")
+    class TddDiceTest {
+        @Test
+        @Tag("Unit-test")
+        @Tag("TDD")
+        @DisplayName("hit dice should return minimum value")
+        void hitDiceShouldReturnMinimumValue(){
+            when(mockRandom.nextInt(20)).thenReturn(0);
 
-        assertEquals(1, result);
-        verify(mockRandom).nextInt(20);
+            DiceRoll hitDice = new RollHitDice(mockRandom);
+            int result = hitDice.roll();
+
+            assertEquals(1, result);
+            verify(mockRandom).nextInt(20);
+        }
+        @Test
+        @Tag("Unit-test")
+        @Tag("TDD")
+        @DisplayName("should return sum of simulated values for axe weapon")
+        void shouldReturnSumofSimulatedValuesForAxeWeapon(){
+            when(mockRandom.nextInt(6)).thenReturn(0, 5);
+
+            DiceRoll attackDice = new RollAttackDice(Weapon.AXE, mockRandom);
+            int result = attackDice.roll();
+
+            assertEquals(7, result);
+            verify(mockRandom, times(2)).nextInt(6);
+        }
     }
 
-    @Test
-    @Tag("Unit-test")
-    @DisplayName("hit dice should return max value")
-    void hitDiceShouldReturnMaxValue(){
-        when(mockRandom.nextInt(20)).thenReturn(19);
+    @Nested
+    @DisplayName("Unit dice test")
+    class UnitDiceTest {
+        @Test
+        @Tag("Unit-test")
+        @DisplayName("hit dice should return max value")
+        void hitDiceShouldReturnMaxValue(){
+            when(mockRandom.nextInt(20)).thenReturn(19);
 
-        DiceRoll hitDice = new RollHitDice(mockRandom);
-        int result = hitDice.roll();
+            DiceRoll hitDice = new RollHitDice(mockRandom);
+            int result = hitDice.roll();
 
-        assertEquals(20, result);
-        verify(mockRandom).nextInt(20);
+            assertEquals(20, result);
+            verify(mockRandom).nextInt(20);
+        }
+
+        @Test
+        @Tag("Unit-test")
+        @DisplayName("should return simulated value for hammer weapon")
+        void shouldReturnSimulatedValueForHammerWeapon() {
+            when(mockRandom.nextInt(12)).thenReturn(6);
+
+            DiceRoll attackDice = new RollAttackDice(Weapon.HAMMER, mockRandom);
+            int result = attackDice.roll();
+
+            assertEquals(7, result);
+            verify(mockRandom, times(1)).nextInt(12);
+        }
+        @Test
+        @Tag("Unit-test")
+        @DisplayName("should return simulated value for sword weapon")
+        void shouldReturnSimulatedValueForSwordWeapon() {
+            when(mockRandom.nextInt(4)).thenReturn(3);
+
+            DiceRoll attackDice = new RollAttackDice(Weapon.SWORD, mockRandom);
+            int result = attackDice.roll();
+
+            assertEquals(12, result);
+            verify(mockRandom, times(3)).nextInt(4);
+        }
+
+        @Test
+        @Tag("Unit-test")
+        @DisplayName("should return simulated value for dagger weapon")
+        void shouldReturnSimulatedValueForDaggerWeapon() {
+            when(mockRandom.nextInt(3)).thenReturn(2);
+
+            DiceRoll attackDice = new RollAttackDice(Weapon.DAGGER, mockRandom);
+            int result = attackDice.roll();
+
+            assertEquals(12, result);
+            verify(mockRandom, times(4)).nextInt(3);
+        }
     }
 
-    @Test
-    @Tag("Unit-test")
-    @DisplayName("should return simulated value for hammer weapon")
-    void shouldReturnSimulatedValueForHammerWeapon() {
-        when(mockRandom.nextInt(12)).thenReturn(6);
 
-        DiceRoll attackDice = new RollAttackDice(Weapon.HAMMER, mockRandom);
-        int result = attackDice.roll();
 
-        assertEquals(7, result);
-        verify(mockRandom, times(1)).nextInt(12);
-    }
 
-    @Test
-    @Tag("Unit-test")
-    @Tag("TDD")
-    @DisplayName("should return sum of simulated values for axe weapon")
-    void shouldReturnSumofSimulatedValuesForAxeWeapon(){
-        when(mockRandom.nextInt(6)).thenReturn(0, 5);
 
-        DiceRoll attackDice = new RollAttackDice(Weapon.AXE, mockRandom);
-        int result = attackDice.roll();
 
-        assertEquals(7, result);
-        verify(mockRandom, times(2)).nextInt(6);
-    }
 
-    @Test
-    @Tag("Unit-test")
-    @DisplayName("should return simulated value for sword weapon")
-    void shouldReturnSimulatedValueForSwordWeapon() {
-        when(mockRandom.nextInt(4)).thenReturn(3);
-
-        DiceRoll attackDice = new RollAttackDice(Weapon.SWORD, mockRandom);
-        int result = attackDice.roll();
-
-        assertEquals(12, result);
-        verify(mockRandom, times(3)).nextInt(4);
-    }
-
-    @Test
-    @Tag("Unit-test")
-    @DisplayName("should return simulated value for dagger weapon")
-    void shouldReturnSimulatedValueForDaggerWeapon() {
-        when(mockRandom.nextInt(3)).thenReturn(2);
-
-        DiceRoll attackDice = new RollAttackDice(Weapon.DAGGER, mockRandom);
-        int result = attackDice.roll();
-
-        assertEquals(12, result);
-        verify(mockRandom, times(4)).nextInt(3);
-    }
 }
