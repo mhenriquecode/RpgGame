83b6b822e40e4ee3addbcc86fb43635a6cd0021b
Pedro Candido <pedrocsalvio@gmail.com>
2025-05-27 18:31:24 -0300

    refactor: adjusting Combat class with combat logic and null verification

--- src/main/java/br/ifsp/web/model/Combat.java
+++ src/main/java/br/ifsp/web/model/Combat.java
@@ -1,46 +1,22 @@
 package br.ifsp.web.model;
 
 import br.ifsp.web.interfaces.ChooseAction;
-import br.ifsp.web.model.actions.ChooseUserAction;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
 
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor(force = true)
 public class Combat {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
-
-    @ManyToOne(optional = false)
-    private RpgCharacter player1;
-
-    @ManyToOne(optional = false)
-    private RpgCharacter player2;
-
-    @ManyToOne
+    private final UUID id;
+    private final RpgCharacter player1;
+    private final RpgCharacter player2;
     private RpgCharacter winner;
 
-    @Transient
     private ChooseAction actionStrategy1;
-
-    @Transient
     private ChooseAction actionStrategy2;
-    @Transient
-    private RpgCharacter finalClone1;
 
-    @Transient
+    private RpgCharacter finalClone1;
     private RpgCharacter finalClone2;
-    @Transient
+
     private Random random = new Random();
 
     public Combat(RpgCharacter player1, ChooseAction actionStrategy1,
@@ -57,6 +33,128 @@ public class Combat {
         this.id = UUID.randomUUID();
         this.player1 = player1;
         this.player2 = player2;
+        this.random = random;
+    }
+
+    public void startCombat(RpgCharacter player1, ChooseAction strategy1, RpgCharacter player2, ChooseAction strategy2) {
+        if (player1 == null) throw new NullPointerException("player1 cannot be null");
+        if (player2 == null) throw new NullPointerException("player2 cannot be null");
+        if (strategy1 == null) throw new NullPointerException("player1 action cannot be null");
+        if (strategy2 == null) throw new NullPointerException("player2 action cannot be null");
+
+        RpgCharacter clone1 = player1.cloneForCombat();
+        RpgCharacter clone2 = player2.cloneForCombat();
+
+        RpgCharacter combatWinner = executeCombat(clone1, strategy1, clone2, strategy2);
+        RpgCharacter actualWinner = (combatWinner == clone1) ? player1 : player2;
+
+        this.setFinalClone1(clone1);
+        this.setFinalClone2(clone2);
+        this.setWinner(actualWinner);
+    }
+
+    private RpgCharacter executeCombat(RpgCharacter player1, ChooseAction strategy1, RpgCharacter player2, ChooseAction strategy2) {
+        RpgCharacter current = getFirstToPlay(player1, player2);
+        RpgCharacter opponent = current == player1 ? player2 : player1;
+
+        while (player1.getHealth() > 0 && player2.getHealth() > 0) {
+            ChooseAction currentStrategy = (current == player1) ? strategy1 : strategy2;
+
+            Turn turn = new Turn(current, opponent, currentStrategy);
+            turn.execute();
+
+            if (opponent.getHealth() <= 0) {
+                return current;
+            }
+
+            RpgCharacter temp = current;
+            current = opponent;
+            opponent = temp;
+        }
+
+        return player1.getHealth() > 0 ? player1 : player2;
+    }
+
+    public RpgCharacter getFirstToPlay(RpgCharacter player1, RpgCharacter player2) {
+        if (player1.getSpeed() > player2.getSpeed()) {
+            return player1;
+        } else if (player2.getSpeed() > player1.getSpeed()) {
+            return player2;
+        } else {
+            return new Random().nextInt(2) == 0 ? player1 : player2;
+        }
+    }
+
+    public RpgCharacter getFirstToPlay(RpgCharacter player1, RpgCharacter player2, Random random) {
+        if (player1.getSpeed() > player2.getSpeed()) {
+            return player1;
+        } else if (player2.getSpeed() > player1.getSpeed()) {
+            return player2;
+        } else {
+            return random.nextInt(2) == 0 ? player1 : player2;
+        }
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public RpgCharacter getPlayer1() {
+        return player1;
+    }
+
+    public RpgCharacter getPlayer2() {
+        return player2;
+    }
+
+    public RpgCharacter getWinner() {
+        return winner;
+    }
+
+    public void setWinner(RpgCharacter winner) {
+        if(winner == null) throw new NullPointerException("winner cannot be null");
+        this.winner = winner;
+    }
+
+    public ChooseAction getActionStrategy1() {
+        return actionStrategy1;
+    }
+
+    public void setActionStrategy1(ChooseAction actionStrategy1) {
+        if(actionStrategy1 == null) throw new NullPointerException("actionStrategy1 cannot be null");
+        this.actionStrategy1 = actionStrategy1;
+    }
+
+    public ChooseAction getActionStrategy2() {
+        return actionStrategy2;
+    }
+
+    public void setActionStrategy2(ChooseAction actionStrategy2) {
+        if(actionStrategy2 == null) throw new NullPointerException("actionStrategy2 cannot be null");
+        this.actionStrategy2 = actionStrategy2;
+    }
+
+    public RpgCharacter getFinalClone1() {
+        return finalClone1;
+    }
+
+    public void setFinalClone1(RpgCharacter finalClone1) {
+        this.finalClone1 = finalClone1;
+    }
+
+    public RpgCharacter getFinalClone2() {
+        return finalClone2;
+    }
+
+    public void setFinalClone2(RpgCharacter finalClone2) {
+        this.finalClone2 = finalClone2;
+    }
+
+    public Random getRandom() {
+        return random;
+    }
+
+    public void setRandom(Random random) {
         this.random = random;
     }
 }
