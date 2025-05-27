3e023a0e70e7c8bec42c3ec4a47dc856dec84333
Pedro Candido <pedrocsalvio@gmail.com>
2025-05-27 18:39:55 -0300

    fix: moving combat service methods to Combat class

--- src/main/java/br/ifsp/web/service/CombatService.java
+++ src/main/java/br/ifsp/web/service/CombatService.java
@@ -20,49 +20,6 @@ import java.util.Random;
 public class CombatService {
     private final CombatLogRepository combatLogRepository;
 
-    public Combat startCombat(RpgCharacter player1, ChooseAction strategy1, RpgCharacter player2, ChooseAction strategy2) {
-        if (player1 == null) throw new NullPointerException("player1 cannot be null");
-        if (player2 == null) throw new NullPointerException("player2 cannot be null");
-        if (strategy1 == null) throw new NullPointerException("player1 action cannot be null");
-        if (strategy2 == null) throw new NullPointerException("player2 action cannot be null");
-        RpgCharacter clone1 = player1.cloneForCombat();
-        RpgCharacter clone2 = player2.cloneForCombat();
-
-        Combat combat = new Combat(player1, strategy1, player2, strategy2);
-
-        RpgCharacter combatWinner = executeCombat(clone1, strategy1, clone2, strategy2);
-        RpgCharacter actualWinner = (combatWinner == clone1) ? player1 : player2;
-
-        combat.setFinalClone1(clone1);
-        combat.setFinalClone2(clone2);
-        combat.setWinner(actualWinner);
-        saveCombatLog(combat);
-
-        return combat;
-    }
-
-    private RpgCharacter executeCombat(RpgCharacter player1, ChooseAction strategy1, RpgCharacter player2, ChooseAction strategy2) {
-        RpgCharacter current = getFirstToPlay(player1, player2);
-        RpgCharacter opponent = current == player1 ? player2 : player1;
-
-        while (player1.getHealth() > 0 && player2.getHealth() > 0) {
-            ChooseAction currentStrategy = (current == player1) ? strategy1 : strategy2;
-
-            Turn turn = new Turn(current, opponent, currentStrategy);
-            turn.execute();
-
-            if (opponent.getHealth() <= 0) {
-                return current;
-            }
-
-            RpgCharacter temp = current;
-            current = opponent;
-            opponent = temp;
-        }
-
-        return player1.getHealth() > 0 ? player1 : player2;
-    }
-
     private void saveCombatLog(Combat combat) {
         CombatLog log = new CombatLog(
                 combat.getPlayer1(),
@@ -73,7 +30,6 @@ public class CombatService {
         combatLogRepository.save(log);
     }
 
-
     public List<CombatHistoryDTO> getCombatHistory() {
         return combatLogRepository.findAll().stream()
                 .map(log -> new CombatHistoryDTO(
@@ -83,26 +39,5 @@ public class CombatService {
                         log.getTimestamp()
                 ))
                 .toList();
-    }
-
-    public RpgCharacter getFirstToPlay(RpgCharacter player1, RpgCharacter player2) {
-        if (player1.getSpeed() > player2.getSpeed()) {
-            return player1;
-        } else if (player2.getSpeed() > player1.getSpeed()) {
-            return player2;
-        } else {
-            return new Random().nextInt(2) == 0 ? player1 : player2;
-        }
-    }
-
-
-    public RpgCharacter getFirstToPlay(RpgCharacter player1, RpgCharacter player2, Random random) {
-        if (player1.getSpeed() > player2.getSpeed()) {
-            return player1;
-        } else if (player2.getSpeed() > player1.getSpeed()) {
-            return player2;
-        } else {
-            return random.nextInt(2) == 0 ? player1 : player2;
-        }
     }
 }
