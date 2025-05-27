e7dae53374096379a5385a6b0d4ebf788af62fc2
Pedro Candido <pedrocsalvio@gmail.com>
2025-05-27 18:51:03 -0300

    feat: creating RpgCharacterEntity for data persistence

---
+++ src/main/java/br/ifsp/web/repository/RpgCharacterEntity.java
@@ -0,0 +1,97 @@
+package br.ifsp.web.repository;
+
+
+import br.ifsp.web.interfaces.SpecialEffect;
+import br.ifsp.web.model.RpgCharacter;
+import br.ifsp.web.model.dice.RollAttackDice;
+import br.ifsp.web.model.dice.RollHitDice;
+import br.ifsp.web.model.enums.ClassType;
+import br.ifsp.web.model.enums.Race;
+import br.ifsp.web.model.enums.Weapon;
+import br.ifsp.web.model.specialEffects.SpecialEffectBerserk;
+import br.ifsp.web.model.specialEffects.SpecialEffectDuelist;
+import br.ifsp.web.model.specialEffects.SpecialEffectPaladin;
+import br.ifsp.web.model.specialEffects.SpecialEffectWarrior;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Random;
+import java.util.UUID;
+
+@Entity(name = "characters")
+@Getter
+@NoArgsConstructor(force = true)
+public class RpgCharacterEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ClassType classType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Race race;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Weapon weapon;
+
+    @JsonIgnore private int maxHealth;
+    @JsonIgnore private int health;
+    @JsonIgnore private int strength;
+    @JsonIgnore private int defense;
+    @JsonIgnore private int speed;
+    @JsonIgnore private int armor;
+    @JsonIgnore private boolean defending = false;
+    @JsonIgnore private boolean hasDodgeBonus = false;
+
+    @Transient @JsonIgnore private RollAttackDice attackDice;
+    @Transient @JsonIgnore private RollHitDice hitDice;
+    @Transient @JsonIgnore private Random random;
+    @Transient @JsonIgnore private SpecialEffect specialEffect;
+
+    public RpgCharacterEntity(String name, ClassType classType, Race race, Weapon weapon) {
+        this.name = name;
+        this.classType = classType;
+        this.race = race;
+        this.weapon = weapon;
+        initializeAttributes();
+        this.attackDice = new RollAttackDice(this.weapon);
+        this.hitDice = new RollHitDice();
+        this.specialEffect = chooseSpecialEffect(classType);
+        this.random = new Random();
+    }
+
+    @PostLoad
+    private void initTransientFields() {
+        this.attackDice = new RollAttackDice(this.weapon);
+        this.hitDice = new RollHitDice();
+        this.random = new Random();
+        this.specialEffect = chooseSpecialEffect(this.classType);
+    }
+
+    public void initializeAttributes() {
+        this.maxHealth = 100 + race.getBonusHealth() + classType.getBonusHealth();
+        this.health = maxHealth;
+        this.strength = 10 + race.getBonusStrength() + classType.getBonusStrength();
+        this.defense = 5 + race.getBonusDefense() + classType.getBonusDefense();
+        this.speed = 4 + race.getBonusSpeed() + classType.getBonusSpeed();
+        this.armor = 10;
+    }
+
+    private SpecialEffect chooseSpecialEffect(ClassType classType){
+        return switch (classType){
+            case BERSERK -> new SpecialEffectBerserk();
+            case DUELIST -> new SpecialEffectDuelist();
+            case PALADIN -> new SpecialEffectPaladin();
+            case WARRIOR -> new SpecialEffectWarrior();
+        };
+    }
+}
