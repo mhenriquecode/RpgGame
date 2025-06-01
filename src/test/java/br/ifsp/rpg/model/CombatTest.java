931b79942bd5053e960ec510de1526a5863d29ae
mhenriquecode <matheucar186@gmail.com>
2025-05-31 23:57:25 -0300

    test: combat ends immediately if player2 starts with zero health for killing while mutant

--- src/test/java/br/ifsp/rpg/model/CombatTest.java
+++ src/test/java/br/ifsp/rpg/model/CombatTest.java
@@ -512,5 +512,26 @@ public class CombatTest {
 
             assertEquals(player2, combat.getWinner());
         }
+        @Test
+        @Tag("Mutation")
+        @Tag("Unit-Test")
+        @DisplayName("combat ends immediately if player2 starts with zero health")
+        void combatEndsImmediatelyIfPlayer2StartsWithZeroHealth() {
+            RpgCharacter player1 = mock(RpgCharacter.class);
+            RpgCharacter player2 = mock(RpgCharacter.class);
+
+            when(player1.getHealth()).thenReturn(10);
+            when(player2.getHealth()).thenReturn(0);
+            when(player1.cloneForCombat()).thenReturn(player1);
+            when(player2.cloneForCombat()).thenReturn(player2);
+
+            ChooseAction strategy1 = mock(ChooseAction.class);
+            ChooseAction strategy2 = mock(ChooseAction.class);
+
+            Combat combat = new Combat(player1, strategy1, player2, strategy2);
+            combat.startCombat(player1, strategy1, player2, strategy2);
+
+            assertEquals(player1, combat.getWinner());
+        }
     }
 }
