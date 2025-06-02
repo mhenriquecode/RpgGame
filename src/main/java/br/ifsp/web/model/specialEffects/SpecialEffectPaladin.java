13952ffd78e666b517a9de6a5f2e9767e5337f88
Pedro Candido <pedrocsalvio@gmail.com>
2025-06-02 03:59:07 -0300

    refactor: special paladin effect for math min

--- src/main/java/br/ifsp/web/model/specialEffects/SpecialEffectPaladin.java
+++ src/main/java/br/ifsp/web/model/specialEffects/SpecialEffectPaladin.java
@@ -6,8 +6,7 @@ import br.ifsp.web.model.RpgCharacter;
 public class SpecialEffectPaladin implements SpecialEffect {
     @Override
     public int applyEffect(RpgCharacter character, int originalDamage) {
-        if(character.getHealth() + originalDamage > character.getMaxHealth()) character.setHealth(character.getMaxHealth());
-        else character.setHealth(character.getHealth() + originalDamage);
+        character.setHealth(Math.min(character.getHealth() + originalDamage, character.getMaxHealth()));
         return originalDamage;
     }
 }
