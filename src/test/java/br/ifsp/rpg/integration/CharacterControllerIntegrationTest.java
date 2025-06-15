a877511d36522e947bf796b3b6317375403f8453
Alan-VSouza <alansouzeesi@gmail.com>
2025-06-14 21:28:08 -0300

    test: add integration test for character creation

---
+++ src/test/java/br/ifsp/rpg/integration/CharacterControllerIntegrationTest.java
@@ -0,0 +1,59 @@
+package br.ifsp.rpg.integration;
+
+import br.ifsp.web.dto.CharacterDTO;
+import br.ifsp.web.model.enums.ClassType;
+import br.ifsp.web.model.enums.Race;
+import br.ifsp.web.model.enums.Weapon;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = br.ifsp.web.DemoAuthAppApplication.class)
+@AutoConfigureMockMvc
+@Tag("ApiTest")
+@Tag("IntegrationTest")
+class CharacterControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private CharacterDTO novoPersonagem() {
+        return new CharacterDTO(
+                null,
+                "Arthas",
+                ClassType.WARRIOR,
+                Race.HUMAN,
+                Weapon.SWORD,
+                100,
+                20,
+                15,
+                10,
+                5
+        );
+    }
+
+    @Test
+    @WithMockUser
+    void deveCriarPersonagemComSucesso() throws Exception {
+        CharacterDTO dto = novoPersonagem();
+
+        mockMvc.perform(post("/api/characters")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists());
+    }
+}
