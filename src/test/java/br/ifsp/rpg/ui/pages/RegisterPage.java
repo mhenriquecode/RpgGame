8b2b7d6c528f7bf2653002a97b180469d6bf6d7f
Turazzi <analivia.turazzi@gmail.com>
2025-06-22 02:35:02 -0300

    test: Add RegisterPage with form inputs and feedback messages

---
+++ src/test/java/br/ifsp/rpg/ui/pages/RegisterPage.java
@@ -0,0 +1,62 @@
+package br.ifsp.rpg.ui.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class RegisterPage extends BasePage{
+
+    @FindBy(id = "register-name")
+    private WebElement nameInput;
+
+    @FindBy(id = "register-lastname")
+    private WebElement lastnameInput;
+
+    @FindBy(id = "register-email")
+    private WebElement emailInput;
+
+    @FindBy(id = "register-password")
+    private WebElement passwordInput;
+
+    @FindBy(id = "register-confirm-password")
+    private WebElement confirmPasswordInput;
+
+    @FindBy(css = "form button[type='submit']")
+    private WebElement registerButton;
+
+    @FindBy(className = "error-message")
+    private WebElement errorMessage;
+
+    @FindBy(className = "success-message")
+    private WebElement successMessage;
+
+    public RegisterPage(WebDriver driver, WebDriverWait wait) {
+
+        super(driver, wait);
+        wait.until(ExpectedConditions.visibilityOf(nameInput));
+
+    }
+
+    public void registerUser(String name, String lastname, String email, String password, String confirmPassword) {
+
+        nameInput.sendKeys(name);
+        lastnameInput.sendKeys(lastname);
+        emailInput.sendKeys(email);
+        passwordInput.sendKeys(password);
+        confirmPasswordInput.sendKeys(confirmPassword);
+        registerButton.click();
+
+    }
+
+    public String getErrorMessage() {
+        return wait.until(ExpectedConditions.visibilityOf(errorMessage)).getText();
+    }
+
+    public String getSuccessMessage() {
+        return wait.until(ExpectedConditions.visibilityOf(successMessage)).getText();
+    }
+
+}
