39d054e12cfef6e811980e486a86b0dc0dedf647
junior220503 <fabianomarochauga@yahoo.com>
2025-06-21 22:52:23 -0300

    test: Added a test that verifies if the hello endpoint returns the right message

--- src/test/java/br/ifsp/rpg/integration/TransactionControllerIntegrationTest.java
+++ src/test/java/br/ifsp/rpg/integration/TransactionControllerIntegrationTest.java
@@ -1,4 +1,34 @@
 package br.ifsp.rpg.integration;
 
-public class TransactionControllerIntegrationTest {
+import br.ifsp.web.DemoAuthAppApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.matchesRegex;
+import static org.hamcrest.Matchers.startsWith;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(
+        classes = DemoAuthAppApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class TransactionControllerIntegrationTest extends BaseApiIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void helloEndpointShouldReturnHelloMessageWhenAuthorized() throws Exception {
+        String token = getAuthToken();
+
+        mockMvc.perform(get("/api/v1/hello")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
+                .andExpect(status().isOk())
+                .andExpect(content().string(startsWith("Hello: ")))
+                .andExpect(content().string(matchesRegex("Hello: [0-9a-f\\-]{36}")));
+    }
 }
