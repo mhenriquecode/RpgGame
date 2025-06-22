d23d85896d716b50645e6819a138012415b1d934
Turazzi <analivia.turazzi@gmail.com>
2025-06-22 02:19:05 -0300

    test: Add base UI test setup with WebDriver and WebDriverWait

---
+++ src/test/java/br/ifsp/rpg/ui/BaseUITest.java
@@ -0,0 +1,40 @@
+package br.ifsp.rpg.ui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseUITest {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected final String baseUrl = "http://localhost:5173";
+
+    @BeforeEach
+    public void setUp() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+        if(driver != null) {
+            driver.quit();
+        }
+
+    }
+
+}
