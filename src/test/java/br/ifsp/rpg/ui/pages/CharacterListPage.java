0b2edf2adba8ff9dee56c8d77496e16fd6681aa3
Alan-VSouza <alansouzeesi@gmail.com>
2025-06-22 12:26:13 -0300

    test: add CharacterFormPage, CharacterListPage and first UI test for character creation

--- src/test/java/br/ifsp/rpg/ui/pages/CharacterListPage.java
+++ src/test/java/br/ifsp/rpg/ui/pages/CharacterListPage.java
@@ -8,12 +8,17 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CharacterListPage extends BasePage {
 
-    @FindBy(id = "create-character-button")
-    private WebElement createCharacterButton;
+    @FindBy(css = "a.characters-create-button")
+    private WebElement criarNovoPersonagemLink;
 
     public CharacterListPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
-        wait.until(ExpectedConditions.visibilityOf(createCharacterButton));
+        wait.until(ExpectedConditions.visibilityOf(criarNovoPersonagemLink));
+    }
+
+    public void clicarCriarNovoPersonagem() {
+        wait.until(ExpectedConditions.visibilityOf(criarNovoPersonagemLink));
+        criarNovoPersonagemLink.click();
     }
 
     public String getHeaderText() {
