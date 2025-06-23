562dfefb8a4f672345c8c006fa83ded4c60c2ee5
Turazzi <analivia.turazzi@gmail.com>
2025-06-22 21:11:55 -0300

    test: Implemented CombatUITest

--- src/test/java/br/ifsp/rpg/ui/Combat/CombatUITest.java
+++ src/test/java/br/ifsp/rpg/ui/Combat/CombatUITest.java
@@ -1,4 +1,56 @@
 package br.ifsp.rpg.ui.Combat;
 
-public class CombatUITest {
+import br.ifsp.rpg.ui.BaseUITest;
+import br.ifsp.rpg.ui.pages.*;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CombatUITest extends BaseUITest {
+
+    private Faker faker;
+    private String email;
+    private String password;
+    private HomePage homePage;
+    private String character1Name;
+    private String character2Name;
+    private CombatPage combatPage;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        faker = new Faker();
+
+        driver.get(baseUrl);
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegisterPage registerPage = loginPage.navigateToRegisterPage();
+        email = faker.internet().emailAddress();
+        password = faker.internet().password(8, 16, true, true);
+        registerPage.registerUser(faker.name().firstName(), faker.name().lastName(), email, password, password);
+        wait.until(ExpectedConditions.urlToBe(baseUrl + "/"));
+        driver.get(baseUrl);
+        homePage = loginPage.login(email, password);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='Sair']")));
+
+        character1Name = faker.name().firstName() + " o Guerreiro";
+        character2Name = faker.name().firstName() + " o Arqueiro";
+
+        CharacterFormPage characterFormPage = homePage.navigateToCreateCharacterPage();
+
+        characterFormPage.createCharacter(character1Name, "ORC", "BERSERK", "SWORD");
+
+        characterFormPage.createCharacter(character2Name, "HUMAN", "PALADIN", "AXE");
+
+        driver.get(baseUrl);
+        combatPage = homePage.navigateToCombatPage();
+    }
+
+
+
 }
